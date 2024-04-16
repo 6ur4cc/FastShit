cbdd2330be208a4d936bd1f9fc4457c65e722106
Burak <144011794+6ur4cc@users.noreply.github.com>
2024-04-16 10:50:42 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,4 +1,4 @@
-module.exports.www_path = "indexdir";
+module.exports.www_path = "public_html";
 module.exports.default_port = 8081;
 module.exports.php_path = "/usr/bin/php";
 module.exports.index_file = "index.php";
